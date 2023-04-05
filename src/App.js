eeb24f0575c7382f6b97cbea4895ae8afd42f6d4
patricia <patricia.g.rojas.97@gmail.com>
2023-04-05 00:21:47 -0600

    Depurando código, utilizo useState, Ternario y Corto circuito - funcion de mostrar y ocultar formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
 import MiOrg from './componentes/MiOrg/MiOrg.js';
 
 function App() {
+
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+
+  //Ternario --> condicion ? seMuestra : noSeMuestra
+  //fragmas --> <></>
+  //corto circuito --> condicion && seMuestra
+  const cambiarEstado = () => {
+    actualizarMostrar(!mostrarFormulario);
+  }
   return (
     <div>
       <Header />
-      <Formulario />
-      <MiOrg />
+      {/*mostrarFormulario === true ? <Formulario /> : <div></div>*/}
+      {/*mostrarFormulario ? <Formulario /> : <div></div> */}
+      {/*mostrarFormulario === true ? <Formulario /> : <></>*/} 
+      {mostrarFormulario && <Formulario />}
+      <MiOrg estado={cambiarEstado} />
     </div>
   );
 }
--- src/componentes/MiOrg/MiOrg.js
+++ src/componentes/MiOrg/MiOrg.js
@@ -1,23 +1,14 @@
-import { useState } from "react"
 import "./MiOrg.css"
 
-const MiOrg = () => {
+const MiOrg = (props) => {
     //Estado - hooks
     //useState
     //useState()
     //const [nombreVariable,funcionActualiza] = useState(valorInicial);
 
-    const [nombre,actualizarNombre] = useState("Harland")
-    const [mostrar,actualizarMostar] = useState(true)
-
-    const manejarClick = () => {
-        console.log("Mostrar/Ocultar elemento", !mostrar)
-        actualizarMostar(!mostrar);
-    }
-
     return <section className="orgSectio">
-                <h3 className="titulo__principal titulo__principal-miorg--modificador">Mi Organización {nombre}</h3>
-                <img src="/img/add.png" alt="boton de agregar" onClick={manejarClick}/>
+                <h3 className="titulo__principal titulo__principal-miorg--modificador">Mi Organización</h3>
+                <img src="/img/add.png" alt="boton de agregar" onClick={props.estado}/>
            </section>
 }
 
