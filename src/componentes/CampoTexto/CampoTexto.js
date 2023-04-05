4354c5acadef47d08ed3be59016642711d7e858b
patricia <patricia.g.rojas.97@gmail.com>
2023-04-04 21:23:49 -0600

    Formulario - Tener campos requeridos

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,11 @@
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
+    console.log(props);
     const placeholderModificado = `${props.placeholder}...`
     return <div className="campo-texto">
                 <label className="label">{props.label}</label>
-                <input className="input" placeholder={placeholderModificado} />
+                <input className="input" placeholder={placeholderModificado} required={props.required}/>
            </div>
 }
 
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -11,12 +11,11 @@ const Formulario = () => {
     }
 
     return <section className="formulario">
-        
                 <form onSubmit={manejarEnvio}>
                     <h2>Rellena el formulario para crear el colaborador.</h2>
-                    <CampoTexto label="Nombre" placeholder="Ingresar nombre" />
-                    <CampoTexto label="Puesto" placeholder="Ingresar puesto"/>
-                    <CampoTexto label="Foto" placeholder="Ingresar enlace de foto"/>
+                    <CampoTexto label="Nombre" placeholder="Ingresar nombre" required={true} />
+                    <CampoTexto label="Puesto" placeholder="Ingresar puesto" required/>
+                    <CampoTexto label="Foto" placeholder="Ingresar enlace de foto" required/>
                     <ListaOpciones />
                     <Boton>
                         Crear
