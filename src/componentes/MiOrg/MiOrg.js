ae829271c1fdc2676274498599a45d209d11f680
patricia <patricia.g.rojas.97@gmail.com>
2023-04-04 23:26:03 -0600

    Comprendiendo - Estado con React (hooks)

--- src/componentes/MiOrg/MiOrg.js
+++ src/componentes/MiOrg/MiOrg.js
@@ -1,9 +1,23 @@
+import { useState } from "react"
 import "./MiOrg.css"
 
 const MiOrg = () => {
+    //Estado - hooks
+    //useState
+    //useState()
+    //const [nombreVariable,funcionActualiza] = useState(valorInicial);
+
+    const [nombre,actualizarNombre] = useState("Harland")
+    const [mostrar,actualizarMostar] = useState(true)
+
+    const manejarClick = () => {
+        console.log("Mostrar/Ocultar elemento", !mostrar)
+        actualizarMostar(!mostrar);
+    }
+
     return <section className="orgSectio">
-                <h3 className="titulo__principal titulo__principal-miorg--modificador">Mi Organización</h3>
-                <img src="/img/add.png" alt="boton de agregar" />
+                <h3 className="titulo__principal titulo__principal-miorg--modificador">Mi Organización {nombre}</h3>
+                <img src="/img/add.png" alt="boton de agregar" onClick={manejarClick}/>
            </section>
 }
 
